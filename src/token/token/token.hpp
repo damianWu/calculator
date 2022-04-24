5850dcc64dd7063fc284ac7b7c8b0f352023a37b
@damianWu <damian.wolonkiewicz@gmail.com>
2022-04-24 18:54:26 +0200

    Added namespace token and class Token

---
+++ src/token/token/token.hpp
@@ -0,0 +1,14 @@
+// Copyright [2022] <@damianWu>
+#ifndef SRC_TOKEN_TOKEN_TOKEN_HPP_
+#define SRC_TOKEN_TOKEN_TOKEN_HPP_
+
+namespace token {
+
+struct Token {
+    char kind{};
+    double value{};
+};
+
+}  // namespace token
+
+#endif  // SRC_TOKEN_TOKEN_TOKEN_HPP_
