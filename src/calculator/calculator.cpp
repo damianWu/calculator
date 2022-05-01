86ab62587996e3639bb5e53976fd813f81e5025f
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-01 11:53:43 +0200

    Export braces to constants

--- src/calculator/calculator.cpp
+++ src/calculator/calculator.cpp
@@ -18,6 +18,11 @@ inline void throw_exception(const std::string& error_msg, char token = ' ') {
 // Receives, holds (only one) and give out token
 TokenStream ts{};
 
+constexpr char open_parenthesis{'('};
+constexpr char close_parenthesis{')'};
+constexpr char open_brace{'{'};
+constexpr char clos_brace{'}'};
+
 }  // namespace
 
 namespace calculator {
