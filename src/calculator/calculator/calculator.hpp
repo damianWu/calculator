d1f958867abbdc29c09c4fad195b6d139917722a
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-01 21:47:47 +0200

    Added function declaration

--- src/calculator/calculator/calculator.hpp
+++ src/calculator/calculator/calculator.hpp
@@ -39,6 +39,7 @@ void verify_closing_bracket(const char closing_bracket);
 double calculate_bitwise_not();
 double calculate_logical_not();
 
+bool is_floating_point_number_token(token::Token* token);
 bool compare_double(const double a, const double b);
 
 }  // namespace calculator
