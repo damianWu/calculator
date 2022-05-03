753b4abccff3143836b8462abe24afb878b815e7
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-03 14:19:22 +0200

    Added three test cases with expected exception

--- tests/calculator.test.cpp
+++ tests/calculator.test.cpp
@@ -424,5 +424,70 @@ TEST(CorrectCalculationBitwiseORTest, ShouldReturnExpectedBitwiseORResult_6) {
     ASSERT_NEAR(given, expected, 0.00001);
 }
 ////////////////////////////////////////////////////////////////////////////////
+TEST(ExceptionCalculatorCalculateFunction, ShouldThrowRuntimeException_1) {
+    EXPECT_THROW(
+        {
+            try {
+                // Given
+                std::istringstream input("2!2");
+                std::cin.rdbuf(input.rdbuf());
+
+                // When
+                calculator::calculate();
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "Function calculator::calculate() "
+                    "throws unexpected token exception. Syntax error. "
+                    "No floating point literal expected.",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
+
+TEST(ExceptionCalculatorCalculateFunction, ShouldThrowRuntimeException_2) {
+    EXPECT_THROW(
+        {
+            try {
+                // Given
+                std::istringstream input("2 2");
+                std::cin.rdbuf(input.rdbuf());
+
+                // When
+                calculator::calculate();
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "Function calculator::calculate() "
+                    "throws unexpected token exception. Syntax error. "
+                    "No floating point literal expected.",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+TEST(ExceptionToken_streamTokenStreamGet, ShouldThrowRuntimeException_1) {
+    EXPECT_THROW(
+        {
+            // Given
+            std::istringstream input("2*2 '");
+            std::cin.rdbuf(input.rdbuf());
+            try {
+                // When
+                calculator::calculate();
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "token_stream::TokenStream::get() "
+                    "throws unknown token exception: '",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
+////////////////////////////////////////////////////////////////////////////////
 
 }  // namespace
