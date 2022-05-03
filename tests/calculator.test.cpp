df3080be5bdfa11d93be0bfd40634dde95a0d257
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-03 15:13:58 +0200

    Split exception and regular tests to two separate files.

--- tests/calculator.test.cpp
+++ tests/calculator.test.cpp
@@ -4,7 +4,6 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
-#include <stdexcept>
 
 namespace {
 
@@ -424,69 +423,17 @@ TEST(CorrectCalculationBitwiseORTest, ShouldReturnExpectedBitwiseORResult_6) {
     ASSERT_NEAR(given, expected, 0.00001);
 }
 ////////////////////////////////////////////////////////////////////////////////
-TEST(ExceptionCalculatorCalculateFunction, ShouldThrowRuntimeException_1) {
-    EXPECT_THROW(
-        {
-            try {
-                // Given
-                std::istringstream input("2!2");
-                std::cin.rdbuf(input.rdbuf());
+TEST(CorrectCalculationNegativeNumbers, ShouldReturnExpectedNegativeResult_1) {
+    // Given
+    std::istringstream input("-1/2; x");
+    std::cin.rdbuf(input.rdbuf());
+    double expected{-0.5};
 
-                // When
-                calculator::calculate();
-            } catch (const std::runtime_error& e) {
-                EXPECT_STREQ(
-                    "Function calculator::calculate() "
-                    "throws unexpected token exception. Syntax error. "
-                    "No floating point literal expected.",
-                    e.what());
-                throw;
-            }
-        },
-        std::runtime_error);
-}
+    // When
+    double given{calculator::calculate()};
 
-TEST(ExceptionCalculatorCalculateFunction, ShouldThrowRuntimeException_2) {
-    EXPECT_THROW(
-        {
-            try {
-                // Given
-                std::istringstream input("2 2");
-                std::cin.rdbuf(input.rdbuf());
-
-                // When
-                calculator::calculate();
-            } catch (const std::runtime_error& e) {
-                EXPECT_STREQ(
-                    "Function calculator::calculate() "
-                    "throws unexpected token exception. Syntax error. "
-                    "No floating point literal expected.",
-                    e.what());
-                throw;
-            }
-        },
-        std::runtime_error);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-TEST(ExceptionToken_streamTokenStreamGet, ShouldThrowRuntimeException_1) {
-    EXPECT_THROW(
-        {
-            // Given
-            std::istringstream input("2*2 '");
-            std::cin.rdbuf(input.rdbuf());
-            try {
-                // When
-                calculator::calculate();
-            } catch (const std::runtime_error& e) {
-                EXPECT_STREQ(
-                    "token_stream::TokenStream::get() "
-                    "throws unknown token exception: '",
-                    e.what());
-                throw;
-            }
-        },
-        std::runtime_error);
+    // Then
+    ASSERT_NEAR(given, expected, 0.00001);
 }
 ////////////////////////////////////////////////////////////////////////////////
 
