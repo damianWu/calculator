317ffbe0f9bad7f520227f31a97c545b412c4bb8
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-06 23:52:56 +0200

    Added new calculator.exception tests

--- tests/calculator.exception.test.cpp
+++ tests/calculator.exception.test.cpp
@@ -4,41 +4,78 @@
 
 #include <gtest/gtest.h>
 
-#include <iostream>
 #include <stdexcept>
 
 #include "token/token.hpp"
 
 namespace {
 
-// Exception tests
-////////////////////////////////////////////////////////////////////////////////
-// TEST(ExceptionCalculatorCalculateFunction,
-//      calculator_throw_if_floating_point_tokenShouldThrowRuntimeException_1) {
-//     EXPECT_THROW(
-//         {
-//             try {
-//                 // Given
-//                 // std::istringstream input("2!2");
-//                 // std::cin.rdbuf(input.rdbuf());
-//                 // token::Token token{FLOATING_POINT_NUMBER, 2};
+TEST(ExceptionCalculator_throw_if_unexpected_token_Function,
+     ShouldThrowUnexpectedTokenRuntimeException_1) {
+    EXPECT_THROW(
+        {
+            try {
+                // Given
+                token::Token token(calculator::FLOATING_POINT_NUMBER, 2);
 
-//                 // When
-//                 calculator::throw_if_floating_point_token(
-//                     new Token{FLOATING_POINT_NUMBER, 2});
-//             } catch (const std::runtime_error& e) {
-//                 EXPECT_STREQ(
-//                     "Function calculator::calculate() "
-//                     "throws unexpected token exception. Syntax error. "
-//                     "No floating point literal expected.",
-//                     e.what());
-//                 throw;
-//             }
-//         },
-//         std::runtime_error);
-// }
-////////////////////////////////////////////////////////////////////////////////
+                // When
+                calculator::throw_if_unexpected_token(token);
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "Function calculator::throw_if_unexpected_token() "
+                    "throws syntax error exception. No floating point literal "
+                    "or bitwise not expected.",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
 
+TEST(ExceptionCalculator_throw_if_unexpected_token_Function,
+     ShouldThrowUnexpectedTokenRuntimeException_2) {
+    EXPECT_THROW(
+        {
+            try {
+                // Given
+                token::Token token('~');
+
+                // When
+                calculator::throw_if_unexpected_token(token);
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "Function calculator::throw_if_unexpected_token() "
+                    "throws syntax error exception. No floating point literal "
+                    "or bitwise not expected.",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
+////////////////////////////////////////////////////////////////////////////////
+TEST(ExceptionCalculatorCalculateUnexpectedFragment,
+     ShouldThrowRuntimeException_1) {
+    EXPECT_THROW(
+        {
+            // Given
+            std::istringstream input("\0");
+            std::cin.rdbuf(input.rdbuf());
+            try {
+                // When
+                calculator::calculate();
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "Function calculate::calculate() throws exception: "
+                    "reached unexpected program fragment. Expected retrun "
+                    "value by while "
+                    "loop. Program forced to exit (probably).",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
 ////////////////////////////////////////////////////////////////////////////////
 
 }  // namespace
