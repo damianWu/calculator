c3058ea32f6620a60246c57a50b48d524db9a220
@damianWu <damian.wolonkiewicz@gmail.com>
2022-05-06 23:55:32 +0200

    Removed unused and addedn new tests for statement.exception

--- tests/statement.exception.test.cpp
+++ tests/statement.exception.test.cpp
@@ -1,58 +1,18 @@
 // Copyright [2022] <@damianWu>
 #include <gtest/gtest.h>
 
-#include <iostream>
 #include <stdexcept>
 
 #include "calculator/calculator.hpp"
 
 namespace {
-// Exception tests
-////////////////////////////////////////////////////////////////////////////////
-// TEST(ExceptionCalculatorCalculateFunction, ShouldThrowRuntimeException_1) {
-//     EXPECT_THROW(
-//         {
-//             try {
-//                 // Given
-//                 std::istringstream input("2!2");
-//                 std::cin.rdbuf(input.rdbuf());
 
-//                 // When
-//                 calculator::calculate();
-//             } catch (const std::runtime_error& e) {
-//                 EXPECT_STREQ("", e.what());
-//                 throw;
-//             }
-//         },
-//         std::runtime_error);
-// }
-////////////////////////////////////////////////////////////////////////////////
-// TEST(ExceptionTokenStreamTokenStreamGet, ShouldThrowRuntimeException_1) {
-//     EXPECT_THROW(
-//         {
-//             // Given
-//             std::istringstream input("2*2'");
-//             std::cin.rdbuf(input.rdbuf());
-//             try {
-//                 // When
-//                 calculator::statement();
-//             } catch (const std::runtime_error& e) {
-//                 EXPECT_STREQ(
-//                     "token_stream::TokenStream::get() "
-//                     "throws unknown token exception: '",
-//                     e.what());
-//                 throw;
-//             }
-//         },
-//         std::runtime_error);
-// }
-////////////////////////////////////////////////////////////////////////////////
-TEST(CorrectCalculationModuloOperator,
+TEST(ExceptionCalculationModuloOperator,
      ShouldThrowRuntimeModuloDivisionByZeroException_1) {
     EXPECT_THROW(
         {
             // Given
-            std::istringstream input("6%0; x");
+            std::istringstream input("6%0+1; x");
             std::cin.rdbuf(input.rdbuf());
             try {
                 // When
@@ -67,6 +27,26 @@ TEST(CorrectCalculationModuloOperator,
         },
         std::runtime_error);
 }
-////////////////////////////////////////////////////////////////////////////////
+
+TEST(ExceptionCalculationDivisionOperator,
+     ShouldThrowRuntimeDivisionByZeroException_1) {
+    EXPECT_THROW(
+        {
+            // Given
+            std::istringstream input("22/0+1; x");
+            std::cin.rdbuf(input.rdbuf());
+            try {
+                // When
+                calculator::grammar::statement();
+            } catch (const std::runtime_error& e) {
+                EXPECT_STREQ(
+                    "Function calculator::term() "
+                    "throws division by zero exception!",
+                    e.what());
+                throw;
+            }
+        },
+        std::runtime_error);
+}
 
 }  // namespace
